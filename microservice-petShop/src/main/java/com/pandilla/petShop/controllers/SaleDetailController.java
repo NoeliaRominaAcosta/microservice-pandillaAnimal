fdb1e48276c040f2a59b56cfad1a91ec1e12853b
NoeliaRominaAcosta <noeliaromina20@gmail.com>
2025-07-12 21:50:55 -0300

    Configuration of the gateway

--- microservice-petShop/src/main/java/com/pandilla/petShop/controllers/SaleDetailController.java
+++ microservice-petShop/src/main/java/com/pandilla/petShop/controllers/SaleDetailController.java
@@ -13,6 +13,7 @@ import java.util.List;
 import java.util.Optional;
 
 @RestController
+@RequestMapping("/SalesDetails")
 public class SaleDetailController {
     @Autowired
     private SalesDetailsService service;
